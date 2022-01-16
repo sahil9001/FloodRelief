8456d5c1e034e68140566c2047d13a5b2de954fc
sahil9001 <sahilsilare@gmail.com>
2022-01-17 00:17:02 +0530

    feat: Added navbar

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", 'sans-serif'],
+        roboto : ["Roboto", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
