15a8ccc0d3db5b9b18a6bdcc0149c6e871b16ddb
Prakhar Asaiya <63089922+prakhar-asaiya@users.noreply.github.com>
2022-01-18 11:07:47 +0530

    Added Footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import Part1 from "./components/Part1";
 import Part2 from "./components/Part2";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <div>
       <NavBar />
       <Part1 />
       <Part2 />
+      <Footer />
     </div>
   );
 }
