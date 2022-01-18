5e1576226ec878f934d585cf907ebab8b296c409
sahil9001 <sahilsilare@gmail.com>
2022-01-18 11:11:15 +0530

    feat: Added logo and title icons

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import Part1 from "./components/Part1";
 import Part2 from "./components/Part2";
+import Part3 from "./components/Part3";
 function App() {
   return (
     <div>
       <NavBar />
       <Part1 />
       <Part2 />
+      <Part3 />
     </div>
   );
 }
