7d9241fe4473691d8330d2e11ae5f426e1f0cd36
sahil9001 <sahilsilare@gmail.com>
2022-01-20 16:34:47 +0530

    feat: Added map

--- src/routes/Dashboard/Dashboard.js
+++ src/routes/Dashboard/Dashboard.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import Footer from "../../components/Footer";
 import FadeIn from "react-fade-in";
+import Leafletmap from "../leafletmap/Leafletmap";
 export default class Dashboard extends Component {
   render() {
     return (
       <FadeIn>
         <div className="container mx-auto my-12">
           <p className="font-inter text-3xl font-bold">Dashboard</p>
+          <Leafletmap />
         </div>
         <Footer />
       </FadeIn>
