70f80e0c4ec4d0ca12bb0b86b77960330a265b4f
Prakhar Asaiya <63089922+prakhar-asaiya@users.noreply.github.com>
2022-01-20 12:25:30 +0530

    changed margins in part 4

--- src/routes/HomePage/Part4.js
+++ src/routes/HomePage/Part4.js
@@ -47,7 +47,6 @@ export default class Part4 extends Component {
                 text-7xl
                 font-extrabold
                 text-center
-                sm:text-left
               "
                   >
                     1
@@ -59,7 +58,6 @@ export default class Part4 extends Component {
                 text-lg
                 sm:text-firstpagecardheading
                 text-center
-                sm:text-left
               "
                   >
                     Go to Dashboard
@@ -76,7 +74,6 @@ export default class Part4 extends Component {
                 text-7xl
                 font-extrabold
                 text-center
-                sm:text-left
               "
                   >
                     2
@@ -89,13 +86,15 @@ export default class Part4 extends Component {
                 sm:text-firstpagecardheading
                 text-center
                 sm:text-left
+                mt-4
+                ml-4
               "
                   >
                     Allow Location Access
                   </p>
                 </div>
                 <div className="flex-1 hidden lg:block transform translate-y-4">
-                  <Svg2 className="mx-auto" />
+                  <Svg2 className="mx-auto ml-4" />
                 </div>
                 <div className="flex-1 py-4 lg:py-0">
                   <p
